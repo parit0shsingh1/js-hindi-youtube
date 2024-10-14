134b2d250734522a866eeb39ec931ec36ad58fd3
parit0shsingh1 <paritoshk1011@gmail.com>
2024-10-14 05:06:15 +0000

    full length

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,3 +4,29 @@
 
 //alert(3+3) //implementable in browser but not in nodejs as browser imlementation is document embedded implementation and nodejs is indepemndent impementation
 
+let name= "Paritosh" //string
+let age = 22 //number
+let isEdited = true //boolean
+
+/*
+    following are the datatypes in javscript:
+    -Number => range: 2^53
+    -bigint =>gives more ange tha number
+    -string
+    -boolean =>true/false
+    -null => standalone value often referred to as an object
+    -undefined=>special type
+    -symbol=> used to define uniqueness
+
+
+    we use typeof to get the type of datatypes
+*/
+console.log(typeof name);//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
+
