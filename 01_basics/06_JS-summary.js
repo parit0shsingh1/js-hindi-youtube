dddee4cd59a9cdcb0b3b237422ebb576b429814c
parit0shsingh1 <paritoshk1011@gmail.com>
2024-11-10 06:54:25 +0000

    Renamed and changes

--- 01_basics/06_JS-summary.js
+++ 01_basics/06_JS-summary.js
@@ -50,3 +50,14 @@ const myFunc = function(){
 
 console.log(typeof myFunc);
 //we use typeof operator/funcion to find the value of a datatype in javascript
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+
+//We have two types of memory in javascript
+/*
+Stack Memory(Stores Primitive Datatypes)
+Stack memory uses copies of values i.e. uses call by value
+Heap Memory(Stores Non-Primitive Datatypes)
+Heap memory uses call by reference
+actual values get updated
+*/
